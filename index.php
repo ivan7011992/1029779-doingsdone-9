fc29af7e195f41b989b1f52ef1b5ca24cc1648f7
gorvod <ivan701-1992@mail.ru>
2019-04-12 16:20:39 +0700

    Добавляем изменения чекбокса и таблицы

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // показывать или нет выполненные задачи
-$show_complete_tasks = rand(0, 1);
+ $show_complete_tasks = rand(0, 1);
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -71,13 +71,17 @@ $show_complete_tasks = rand(0, 1);
                     </nav>
 
                     <label class="checkbox">
+
+                       
+
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks==1):?> checked <?php endif?>  >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
 
                 <table class="tasks">
+
                     <tr class="tasks__item task">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
@@ -92,6 +96,19 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
+                    <?php if( $show_complete_tasks==1):?> 
+
+                        <tr class="tasks__item task task--completed">
+                            <td class="task__select">
+                                <label class="checkbox task__checkbox">
+                                    <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                    <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                                </label>
+                            </td>
+                            <td class="task__date">10.10.2019</td>
+                            <td class="task__controls"></td>
+                        </tr>
+                        <?php endif?>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
                 </table>
             </main>
